a03c31458e4ce5029a25cb326843a7edfa715394
Shiy <644424778@qq.com>
2018-10-24 18:16:06 +0800

    xxxxxxxx

--- update_sql.sql
+++ update_sql.sql
@@ -1,4 +1,5 @@
 alter table pig_user add `name` varchar(20) not null comment '姓名';
 alter table pig_user add `tel` varchar(20) not null comment '手机号';
 alter table pig_user add `email` varchar(60) not null comment '邮箱';
+
 alter table pig_permission add `show` enum(yes,no) not null default 'yes' comment '是否显示';
